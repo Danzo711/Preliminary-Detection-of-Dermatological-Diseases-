21b0a56c6dcb7d04bf7bc015691d8d336271e2a8
Danzo711 <117014244+Danzo711@users.noreply.github.com>
2024-07-31 14:59:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Preliminary-Detection-of-Dermatological-Diseases-
+# skin_disease_detection_using_cnns
+Using CNNs for skin disease detection
+
+A guide on how to setup the project locally<br>
+Clone the Repositort using SSH/HTTPS<br>
+Install dependencies mentioned in requirements.txt<br>
+Use pip install -r requirements.txt<br>
+Run app.py <br>
+It will start a local server on port 5000 by default<br>
+Add valid login credentials in the mail.py file <br>
---
+++ mail.py
@@ -0,0 +1,40 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(user_email, information):
+    smtp_server = 'smtp-relay.brevo.com'
+    smtp_port = 587  
+    
+    # Your Yahoo Mail email address and password
+    smtp_username = ''
+    smtp_password = ''
+
+    msg = MIMEMultipart()
+    msg['From'] = smtp_username
+    msg['To'] = user_email
+    msg['Subject'] = 'Skin Disease Prediction Information'
+
+    # Add the information to the email body
+    body = f"Here is the information you requested:\n {information}\n\nThank you for using our service!\nStay Safe!\nRegards Team Inno_Minds"
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        # Create an SMTP connection with TLS (STARTTLS)
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+
+        # Login to your Yahoo Mail account
+        server.login(smtp_username, smtp_password)
+        print("logged in")
+        # Send the email
+        server.sendmail(smtp_username, user_email, msg.as_string())
+        
+        # Close the SMTP server
+        server.quit()
+        print("Email sent successfully")
+    except Exception as e:
+        print("Error sending email:", str(e))
+
+# Example usage:
+# send_email('virendrasinhkhuman@gmail.com', 'This is the information you requested')
